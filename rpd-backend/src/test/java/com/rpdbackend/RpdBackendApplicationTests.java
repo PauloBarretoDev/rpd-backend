f552cab2e5109ac2460b6c50f8e2cde373d87b0f
Paulo Barreto Neto <paulobarreto.dev@gmail.com>
2023-11-29 19:49:16 -0300

    criação de pastas

---
+++ rpd-backend/src/test/java/com/rpdbackend/RpdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
